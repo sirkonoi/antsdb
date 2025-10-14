8fb9185fe43e59a60203e6c09ed161bef605b443
konoi <mattia.rotteri@outlook.com>
2025-10-14 18:41:43 +0200

    PUT revision, specie

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import *
 from . import models, schemas
 from sqlalchemy.orm import Session
 from .database import engine, get_db
@@ -13,6 +13,7 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to AntsDB API!"}
 
+# GET species list
 @app.get("/species")
 def get_all_species(db: Session = Depends(get_db)):
     species = (db.query(models.Species).all())
@@ -29,6 +30,7 @@ def get_all_species(db: Session = Depends(get_db)):
 
     return result    
 
+# GET specie information
 @app.get("/species/{species_id}")
 def get_species(species_id: int, db: Session = Depends(get_db)):
     species = (db.query(models.Species).filter(models.Species.id == species_id).first())
@@ -60,7 +62,7 @@ def get_specie_revision(species_id: int, db: Session = Depends(get_db)):
     return result    
        
 # POST requests
-
+# POST specie --> create new specie
 @app.post("/species")
 def create_species(specie: schemas.SpeciesCreate, db: Session = Depends(get_db)):
 
@@ -79,7 +81,7 @@ def create_species(specie: schemas.SpeciesCreate, db: Session = Depends(get_db))
 
     return {"id": new_specie.id, "scientific_name": new_specie.scientific_name}
 
-
+# POST specie revision --> create new revision
 @app.post("/species/{species_id}/revision")
 def create_revision(species_id: int, revision: schemas.SpeciesRevisionCreate, db: Session = Depends(get_db)):
     try:
@@ -95,3 +97,57 @@ def create_revision(species_id: int, revision: schemas.SpeciesRevisionCreate, db
         return {"id": new_rev.id, "author": new_rev.author_id, "content": new_rev.content, "approved": new_rev.approved}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# DELETE requests
+# DELETE specie
+@app.delete("species/delete/{species_id}")
+def delete_specie(species_id: int, db: Session = Depends(get_db)):
+    specie = db.query(models.Species).filter(species_id == models.Species.id).first()
+
+    if not specie:
+        raise HTTPException(status_code=404, detail="Species not found")
+    
+    db.delete(specie)
+    db.commit()
+
+    return {"message": f"Species {species_id} deleted successfully"}
+
+# DELETE revision
+@app.delete("species/{species_id}/revision/del")
+def delete_specie(revision_id: int, db: Session = Depends(get_db)):
+    revision = db.query(models.SpeciesRevision).filter(revision_id == models.SpeciesRevision.id).first()
+
+    if not revision:
+        raise HTTPException(status_code=404, detail="Species not found")
+    
+    db.delete(revision)
+    db.commit()
+
+    return {"message": f"Species Revision {revision} deleted successfully"}
+
+#PUT requests
+#PUT specie --> update specie
+@app.put("species/{species_id}/update")
+def update_species(species_id: int, species: schemas.SpeciesCreate, db: Session = Depends(get_db)):
+    specie = db.query(models.Species).filter(models.Species.id == species_id).first()
+
+    if not specie:
+        raise HTTPException(status_code=404, detail="Species not found")
+
+    specie.update(species.model_dump())
+    db.commit()
+
+    return {"message": "Species updated successfully", "id": species_id}
+
+#PUT revision --> update revision
+@app.put("species/{species_id}/{revision_id}update")
+def update_species(species_id: int, revision_id: int, species: schemas.SpeciesCreate, db: Session = Depends(get_db)):
+    revision = db.query(models.Species).filter(models.SpeciesRevision.species_id == species_id).filter(models.SpeciesRevision.id == revision_id).first()
+
+    if not revision:
+        raise HTTPException(status_code=404, detail="Species/Revision not found")
+
+    revision.update(species.model_dump())
+    db.commit()
+
+    return {"message": "Revision updated successfully", "id": species_id}
